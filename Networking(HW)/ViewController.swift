27fcaaf31d88974a3362196f697121ad346d624d
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-03-20 16:31:17 +0500

    Initial Commit

---
+++ Networking(HW)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking(HW)
+//
+//  Created by Bektemur Mamashayev on 20/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
